1fec00fa9172dff163c54dbd414ff0e22974719f
James Eland <s210209@students.aie.edu.au>
2021-02-08 10:53:14 +1100

    hi

---
+++ AIE_03_CalculateAverage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AIE_03_CalculateAverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

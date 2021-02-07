2b39847ad7bdea1731782af0d67314d2cbe73060
James Eland <s210209@students.aie.edu.au>
2021-02-08 10:51:19 +1100

    hi

---
+++ AIE_01_GetTwoLenghts/AIE_01_GetTwoLenghts/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace AIE_01_GetTwoLenghts
+{
+    class Program
+    {
+        static int [] GetTwoLengths(string str1, string str2)
+        {
+            int[] lenghts = new int[2];
+            lenghts [0] = str1.Length;
+            lenghts [1] = str2.Length;
+            return lenghts;
+
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+            var strLengths1 = GetTwoLengths("hello", "world");
+            var strLengths2 = GetTwoLengths("", "hello world");
+            var strLengths3 = GetTwoLengths("aaaa", "bbbbbbbbbbbbbbb");
+
+            Console.WriteLine(strLengths1[0]); // 5
+            Console.WriteLine(strLengths1[1]); // 5
+
+            Console.WriteLine(strLengths2[0]); // 0
+            Console.WriteLine(strLengths2[1]); // 11
+
+            Console.WriteLine(strLengths3[0]); // 4
+            Console.WriteLine(strLengths3[1]); // 15
+        }
+    }
+}
---
+++ AIE_02_NeedleInTheHaystack/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AIE_02_NeedleInTheHaystack
+{
+    class Program
+    {
+        static string[] FindNeedle(str1);
+
+
+
+        static void Main(string[] args)
+        {
+            var strings = new string[] { "hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk" };
+
+            var needleLocation = FindNeedle(strings);
+
+            // TODO: output to the console.
+        }
+    }
+}

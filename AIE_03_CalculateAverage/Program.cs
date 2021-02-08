b2146917a36eb440f90434f22c070ee7e2442b9a
James Eland <s210209@students.aie.edu.au>
2021-02-08 11:58:17 +1100

    hello

--- AIE_03_CalculateAverage/Program.cs
+++ AIE_03_CalculateAverage/Program.cs
@@ -4,9 +4,15 @@ namespace AIE_03_CalculateAverage
 {
     class Program
     {
+        static int =  CalculateAverage(int a);
+
+        
+        // TODO: Write the CalculateAverage function here
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var numbers = new int[] { 10, 3, 6, 6, 4, 8, 1, 7 };
+            Console.WriteLine(CalculateAverage(numbers));
         }
     }
 }
